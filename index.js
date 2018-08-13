981233b6450032a343038adf9d30944ad6674928
Olha Melska <olya.melska@gmail.com>
2018-08-13 20:01:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,26 +29,26 @@ function distanceTravelledInFeet(block1, block2){
 }
 
 function calculatesFarePrice(block1, block2){
-    if(block1>block2){
-      let feet = (block1 - block2)*264;
-    if(feet < 400){
-      return 0;
-    }
-    else if(feet > 400 && feet < 2000){
-      return feet*2;
-    }
-    else if(feet > 2000 && feet < 2500){
-      return 500;
-    }
-    else if(feet > 2500){
-      return 
-    }
-      
-    }
+  let feet;
+  
+  if(block1>block2){
+      feet= (block1 - block2)*264;
   }
   else if(block2>block1){
-    return (block2 - block1)*264;
+    feet = (block2 - block1)*264;
   }
   
+  if(feet < 400){
+    return 0;
+  }
+  else if(feet > 400 && feet < 2000){
+    return feet*2;
+  }
+  else if(feet > 2000 && feet < 2500){
+    return 500;
+  }
+  else if(feet > 2500){
+    return 'cannot travel that far';
+  }
   
 }
