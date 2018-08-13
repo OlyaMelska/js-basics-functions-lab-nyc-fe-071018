d0a6ec3758c4c3a73c5d5018a979e873c2c006b8
Olha Melska <olya.melska@gmail.com>
2018-08-13 19:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  if(block<42){
+    return 42-block;
+  }
+  else if(block>42){
+    return block-42;
+  }
+}
+
+function distanceFromHqInFeet(feet){
+  return feet * 264;
+  
+}
