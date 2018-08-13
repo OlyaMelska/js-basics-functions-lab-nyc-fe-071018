d7dcda1c20b023697f3a451a9a4805a53ae7653f
Olha Melska <olya.melska@gmail.com>
2018-08-13 19:59:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,13 @@ function calculatesFarePrice(block1, block2){
     }
     else if(feet > 400 && feet < 2000){
       return feet*2;
-    else if()
+    }
+    else if(feet > 2000 && feet < 2500){
+      return 500;
+    }
+    else if(feet > 2500){
+      return 
+    }
       
     }
   }
